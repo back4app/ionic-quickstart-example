1e27ed5f4491296a06affdeb22cee2996811a047
Max Gomes <maxgomes92@live.com>
2018-04-21 19:18:06 -0300

    Add parse installation save

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,35 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {NavController, Platform} from 'ionic-angular';
+import Parse from 'parse';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  result: string;
 
-  constructor(public navCtrl: NavController) {
+  constructor(
+    public navCtrl: NavController,
+    public platform: Platform,
+  ) {
+    Parse.initialize("YOUR-APP-ID", "YOUR-JS-KEY");
+    Parse.serverURL = 'https://parseapi.back4app.com/';
 
+    let install = new Parse.Installation();
+    install.set("deviceType", this.platform.platforms().toString());
+
+    install.save(null, {
+      success: (install) => {
+        // Execute any logic that should take place after the object is saved.
+        this.result = 'New object created with objectId: ' + install.id;
+      },
+      error: (install, error) => {
+        // Execute any logic that should take place if the save fails.
+        // error is a Parse.Error with an error code and message.
+        this.result = ('Failed to create new object, with error code:' + error.message.toString());
+      }
+    });
   }
 
 }
